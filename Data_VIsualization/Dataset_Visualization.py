7fb1eb30616be474f3470876bd40bb2487b91f70
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-05 19:14:34 +0545

    A practical visualization on dataset for housing taking indicators as Area vs Price.

---
+++ Data_VIsualization/Dataset_Visualization.py
@@ -0,0 +1,18 @@
+# Working on dataset visualization on different indicators or features.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Datasets/Housing.csv')
+print(dataset.head())
+
+
+x_data = dataset['area']
+y_data = dataset['price']
+
+plt.xlabel('Area')
+plt.ylabel('Price')
+plt.title('Area vs Price')
+
+plt.scatter(x_data, y_data, c='#00f', marker = "*", alpha=0.5)
+plt.show()
