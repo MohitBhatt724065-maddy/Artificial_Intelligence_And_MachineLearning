f7d7cc9278eb883ceb27c78f9e75077205fc4cb3
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-07 11:18:17 +0545

    Weight versus Year graph.

--- Data_VIsualization/Fourth_Visualization.py
+++ Data_VIsualization/Fourth_Visualization.py
@@ -8,8 +8,11 @@ y_data = [random.randint(50,90) for _ in range (15)] # Since we use _ instead of
 
 print(x_data, y_data)
 
-plt.scatter(x_data, y_data, c='#00f', marker="*", alpha=0.5)
-plt.show()
-
+'''plt.scatter(x_data, y_data, c='#00f', marker="*", alpha=0.5)
+plt.show()'''
+plt.xlabel('Year')
+plt.ylabel('Weight')
+plt.title('Weight vs Year')
 plt.plot(x_data, y_data)
 plt.show()
+
