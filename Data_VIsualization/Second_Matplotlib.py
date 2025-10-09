0fba0d18edd5c8fd3235f7effd3e2bd7cc4498ca
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-09 21:58:27 +0545

    Simple visualization using the data from dataset using scatter plot.

--- Data_VIsualization/Second_Matplotlib.py
+++ Data_VIsualization/Second_Matplotlib.py
@@ -1,39 +1,10 @@
-#Second Visualization using matplotlib.
+# Basic visualization on line plot.
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-X_Data = np.random.random(1000) * 100
-Y_Data = np.random.random(1000) * 100
+x_data = np.linspace(0, 10*np.pi, 1000)
+y_data = np.sin(x_data)
 
-# Visualizing with scatter plot.
-plt.scatter(X_Data, Y_Data, c='#00f', marker="o", alpha=0.5)
-plt.show()
-
-# Visualizing with bar plot
-x2_data = np.arange(10)
-y2_data = np.random.uniform(0, 10, 10)
-
-plt.bar(x2_data, y2_data)
-plt.show()
-
-# Visualizing with heatmap.
-x3_data = np.random.uniform(0, 1, (8,8))
-plt.imshow(x3_data)
-plt.show()
-
-
-# Visualizing contour plot.
-x4_data = np.random.uniform(0, 1, (8,8))
-plt.contourf(x4_data)
-plt.show()
-
-# Visualizing pie chart.
-x5_data = np.random.uniform(0, 1, 4)
-plt.pie(x5_data)
-plt.show()
-
-
-# Visualizing histogram:-
-x6_data = np.random.normal(0, 1, 100)
-plt.hist(x6_data)
-plt.show()
+plt.plot(x_data, y_data)
+plt.show(  )
--- Data_VIsualization/Third_Matplotlib.py
+++ Data_VIsualization/Third_Matplotlib.py
@@ -1,1 +1,16 @@
-#Third visualization using matplotib.
+# Using Data From Dataset For Visualization.
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Datasets/Housing.csv')
+print(dataset)
+
+x_data = dataset['area']
+y_data = dataset['price']
+
+fig , ax = plt.subplots()
+
+ax.scatter(x_data, y_data)
+plt.show()
