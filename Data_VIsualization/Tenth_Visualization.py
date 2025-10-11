7178b2fc6dff180eff3505419027c02fe6884460
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-11 09:11:33 +0545

    Understanding error plot and bar plot.

---
+++ Data_VIsualization/Tenth_Visualization.py
@@ -0,0 +1,11 @@
+# Understanding error and bar plot.
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_data = np.arange(5)
+y_data = np.random.uniform(0, 1, 5)
+print(x_data)
+print(y_data)
+fig, ax = plt.subplots()
+ax.errorbar(x_data, y_data, y_data/4)
+plt.show()
